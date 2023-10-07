711aee93a9ff9d9af22327c328e992f23b7f06a9
LOOIZIJIAN <looizijiann@gmail.com>
2023-10-07 16:00:18 +0800

    grabDataFromMBD

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import Featured from "@/components/Featured";
 import Hearder from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
-export default function HomePage() {
+export default function HomePage({product}) {
   return(
     <div>
       <Hearder/>
-      <Featured/>
+      <Featured product={product}/>   
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = '651f78824a83d693d7039520';
+  await mongooseConnect();
+  const product = await Product.findById(featuredProductId);
+  return{
+    props: {product: JSON.parse(JSON.stringify(product))},
+  }
 }
