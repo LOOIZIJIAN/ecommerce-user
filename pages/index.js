bd5a7f29620629b9e1c248f49ae16a228383cff7
LOOIZIJIAN <looizijiann@gmail.com>
2024-02-08 11:35:42 +0800

    try enhance pfm

--- pages/index.js
+++ pages/index.js
@@ -4,38 +4,42 @@ import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 import { useSession, signIn, signOut } from "next-auth/react";
 import dynamic from "next/dynamic";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { useRouter } from "next/router";
 import Chatbox from "@/components/ChatBox";
 import { Slide } from "@/models/Slide";
 import Footer from "@/components/Footer";
 
 const Container = styled.div`
-    margin-top: 4%;
-    /* margin-left: -8px; */
-    place-content: center;  //  combines of align-content and justify-content
-    /* width: 101.2%; */
-    width: 95%;
-    height: 517px;
-    // background: linear-gradient(285deg, #000 58.94%, rgba(0, 0, 0, 0.00) 113.07%, rgba(0, 0, 0, 0.11) 113.07%);
-    display: flex;
-    flex-direction: row;
-    gap: 50px;
+  margin-top: 4%;
+  /* margin-left: -8px; */
+  place-content: center; //  combines of align-content and justify-content
+  /* width: 101.2%; */
+  width: 95%;
+  height: 517px;
+  // background: linear-gradient(285deg, #000 58.94%, rgba(0, 0, 0, 0.00) 113.07%, rgba(0, 0, 0, 0.11) 113.07%);
+  display: flex;
+  flex-direction: row;
+  gap: 50px;
 `;
 
 export default function HomePage({ newProducts, slide }) {
-  const SlideShow = dynamic(() => import("@/components/SlideShow"), { ssr: false });
-  const Header = dynamic(() => import('@/components/Header'), { ssr: false });
-  const UserProfile = dynamic(()=> import('@/components/UserProfile'), {ssr: false});
-  const {data: session} = useSession();
-  console.log('indexSlide:'+slide);
+  const SlideShow = dynamic(() => import("@/components/SlideShow"), {
+    ssr: false,
+  });
+  const Header = dynamic(() => import("@/components/Header"), { ssr: false });
+  const UserProfile = dynamic(() => import("@/components/UserProfile"), {
+    ssr: false,
+  });
+  const { data: session } = useSession();
+  console.log("indexSlide:" + slide);
 
   if (!session) {
     return (
       <>
-        <Header session={false}/>
+        <Header session={false} />
         <Container>
-          <SlideShow slides={slide}/>
+          <SlideShow slides={slide} />
           <Login />
         </Container>
 
@@ -48,13 +52,13 @@ export default function HomePage({ newProducts, slide }) {
   }
   return (
     <>
-      <Header session={true}/>
+      <Header session={true} />
       <Container>
-        <SlideShow slides={slide}/>
+        <SlideShow slides={slide} />
         <UserProfile />
       </Container>
       <NewProducts products={newProducts} />
-      <Chatbox/>
+      <Chatbox />
       <Footer />
     </>
   );
@@ -62,12 +66,15 @@ export default function HomePage({ newProducts, slide }) {
 
 export async function getServerSideProps() {
   await mongooseConnect();
-  const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
+  const newProducts = await Product.find({}, null, {
+    sort: { _id: -1 },
+    limit: 10,
+  });
   const slide = await Slide.find();
-  return{
+  return {
     props: {
-      newProducts: JSON.parse(JSON.stringify(newProducts)), 
-      slide: JSON.parse((JSON.stringify(slide))),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+      slide: JSON.parse(JSON.stringify(slide)),
     },
   };
 }
