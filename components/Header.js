ca7244e7feaaeb323d12fa134fef0de48dbbee70
Cresswell-0301 <129597429+Cresswell-0301@users.noreply.github.com>
2024-02-19 17:14:50 +0800

    updated

--- components/Header.js
+++ components/Header.js
@@ -180,7 +180,7 @@ const DropDisplay = styled.div`
   display: none;
   /* width: 92.5%; */
   /* width: 8.23%; */
-  width: 7.50%;
+  width: 8%;
   min-height: 100px;
   height: auto;
   z-index: 2;
@@ -190,7 +190,7 @@ const DropDisplay = styled.div`
   margin-left: -11px;
   position: fixed;
   /* top: 66px; */
-  top: 62px;
+  top: 66px;
 `;
 
 const SharedBtn = styled.div`
