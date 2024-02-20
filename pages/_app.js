cd4ba51dd1a90bfb10ccc2f809d3c6c7c829c97f
LOOIZIJIAN <looizijiann@gmail.com>
2024-02-20 15:43:21 +0800

    add: web logo and name

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import { CartContextProvider } from "@/components/CartContext";
 import { createGlobalStyle } from "styled-components";
 import { SessionProvider } from "next-auth/react";
-import { Toaster } from 'react-hot-toast';
-// import '@/styles/globals.css';
+import { Toaster } from "react-hot-toast";
+import Head from "next/head"; // Import Head from next/head
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
@@ -16,16 +16,32 @@ const GlobalStyles = createGlobalStyle`
   }
 
 `;
-export default function App({ Component, pageProps: {session, ...pageProps} }) {
-  return(
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
     <>
-        <GlobalStyles/>
-        <Toaster />
-        <CartContextProvider>
-          <SessionProvider session={session}>
-            <Component {...pageProps} />
-          </SessionProvider> 
-        </CartContextProvider>
+      <Head>
+        <title>DIRECT Phone Accessory</title>
+        <link
+          rel="icon"
+          href="Screenshot 2023-09-11 152049-photoaidcom-cropped.png"
+        />
+      </Head>
+      <GlobalStyles />
+      <Toaster />
+      <header>
+        {/* Your logo and website name */}
+        <img src="/your-logo.png" alt="Logo" />
+        <h1>Your Website Name</h1>
+      </header>
+      <CartContextProvider>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </CartContextProvider>
     </>
   );
 }
